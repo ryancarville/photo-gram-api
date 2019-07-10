4a8987f26a0026294eff49fcb228bd0777d0d4eb
Ryan Carville <ryancarville@gmail.com>
2019-07-09 17:25:27 -0700

    updated cors

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 app.use(morgan(morganOption));
-app.use(cors());
+app.use(cors({ origin: CLIENT_ORIGIN }));
 app.use(helemt());
 
 app.get('/api/*', (req, res) => {
