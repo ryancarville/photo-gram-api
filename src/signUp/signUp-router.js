594d7333ff3634c93fc12fbdca9caa26c83fe62d
Ryan Carville <ryancarville@gmail.com>
2019-07-10 13:51:03 -0700

    working on signup route

--- src/signUp/signUp-router.js
+++ src/signUp/signUp-router.js
@@ -5,41 +5,42 @@ const signUpRouter = express.Router();
 const jsonBodyParser = express.json();
 
 signUpRouter.post('/', jsonBodyParser, (req, res, next) => {
-	const { full_name, user_name, email, password, nickname } = req.body;
+	return res.json({ ok: 'signup route' });
+	// const { full_name, user_name, email, password, nickname } = req.body;
 
-	for (const field of ['full_name', 'user_name', 'email', 'password'])
-		if (!req.body[field])
-			return res
-				.status(400)
-				.json({ error: `Missing '${field}' in request body'` });
+	// for (const field of ['full_name', 'user_name', 'email', 'password'])
+	// 	if (!req.body[field])
+	// 		return res
+	// 			.status(400)
+	// 			.json({ error: `Missing '${field}' in request body'` });
 
-	const passwordError = SignUpService.validatePassword(password);
-	if (passwordError) return res.status(400).json({ error: passwordError });
+	// const passwordError = SignUpService.validatePassword(password);
+	// if (passwordError) return res.status(400).json({ error: passwordError });
 
-	SignUpService.hasUserWithUserName(req.app.get('db'), user_name)
-		.then(hasUserWithUserName => {
-			if (hasUserWithUserName)
-				return res.status(400).json({ error: 'Username already taken.' });
+	// SignUpService.hasUserWithUserName(req.app.get('db'), user_name)
+	// 	.then(hasUserWithUserName => {
+	// 		if (hasUserWithUserName)
+	// 			return res.status(400).json({ error: 'Username already taken.' });
 
-			return SignUpService.hashPassword(password).then(hashedPassword => {
-				const newUser = {
-					email,
-					user_name,
-					password: hashedPassword,
-					full_name,
-					nickname,
-					date_created: 'now()'
-				};
-				return SignUpService.insertUser(req.app.get('db'), newUser).then(
-					user => {
-						res
-							.status(201)
-							.location(path.posix.join(req.originalUrl, `/${user.id}`))
-							.json(SignUpService.serializeUser(user));
-					}
-				);
-			});
-		})
-		.catch(next);
+	// 		return SignUpService.hashPassword(password).then(hashedPassword => {
+	// 			const newUser = {
+	// 				email,
+	// 				user_name,
+	// 				password: hashedPassword,
+	// 				full_name,
+	// 				nickname,
+	// 				date_created: 'now()'
+	// 			};
+	// 			return SignUpService.insertUser(req.app.get('db'), newUser).then(
+	// 				user => {
+	// 					res
+	// 						.status(201)
+	// 						.location(path.posix.join(req.originalUrl, `/${user.id}`))
+	// 						.json(SignUpService.serializeUser(user));
+	// 				}
+	// 			);
+	// 		});
+	// 	})
+	// 	.catch(next);
 });
 module.exports = signUpRouter;
