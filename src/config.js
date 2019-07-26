c7b722e29cb49ba6e00659b8714f9814cc5db172
Ryan Carville <ryancarville@gmail.com>
2019-07-25 21:36:51 -0700

    changed exipiry time

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
 	NODE_ENV: process.env.NODE_ENV || 'development',
 	DB_URL: process.env.DB_URL || 'postgresql://ryancarville@127.0.0.1/photoGram',
 	JWT_SECRET: process.env.JWT_SECRET || 'thisIsMyJWTSecret',
-	JWT_EXPIRY: process.env.JWT_EXPIRY || '20s'
+	JWT_EXPIRY: process.env.JWT_EXPIRY || '20m'
 };
