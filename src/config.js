bfd31f631426a64e25e94b16842ca42d57b33d2f
Ryan Carville <ryancarville@gmail.com>
2019-07-29 20:52:28 -0700

    deploying

--- src/config.js
+++ src/config.js
@@ -2,7 +2,8 @@ module.exports = {
 	PORT: process.env.PORT || 8000,
 	NODE_ENV: process.env.NODE_ENV || 'development',
 	CLIENT_ORIGIN: '*',
-	DB_URL: process.env.DB_URL || 'postgresql://ryancarville@127.0.0.1/photoGram',
+	DB_URL:
+		process.env.DATABASE_URL || 'postgresql://ryancarville@127.0.0.1/photoGram',
 	JWT_SECRET: process.env.JWT_SECRET || 'thisIsMyJWTSecret',
 	JWT_EXPIRY: process.env.JWT_EXPIRY || '1200000'
 };
